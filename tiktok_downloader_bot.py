9cfbdce95261d80c25e108210bb2338aa4f27680
prohor237 <BROWSER-SOFT@YA.RU>
2025-06-02 22:32:30 +0300

    Add files via upload

--- tiktok_downloader_bot.py
+++ tiktok_downloader_bot.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 import logging
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
